845d27bcef9667f16376b6e45d55afe4ff3afd23
bobo <bobowg@163.com>
2024-10-07 22:39:55 +0800

    账户和卡片业务添加，基本完成。

---
+++ app/src/main/java/com/example/bindomobile/domain/usecases/feature_app_lock/AuthenticateWithPinUseCase.kt
@@ -0,0 +1,16 @@
+package com.example.bindomobile.domain.usecases.feature_app_lock
+
+/**
+ * @author bobo
+ * @project BindoMobile
+ * @date 2024/10/7
+ * @time 下午10:36
+ * @month_full 十月
+ * @day 07
+ * @day_full 星期一
+ * @minute 36
+ */
+class AuthenticateWithPinUseCase(
+
+) {
+}
